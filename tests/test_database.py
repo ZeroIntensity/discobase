5a73c498c31806cc91adced0dc94def2e0808fb4
ZeroIntensity <zintensitydev@gmail.com>
2024-07-20 12:09:28 -0400

    Add test, move to database.py, and add discord.py as a dependency.

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,8 +1,10 @@
 import discobase
 
 
-# This is a sample test
-def test_foo():
+def test_version():
     assert isinstance(discobase.__version__, str)
     assert discobase.__license__ == "MIT"
-    assert 1 != 2
+
+
+def test_creation():
+    db = discobase.Database("test")
