e13f8d0b98a9c6423109a8bc081be629c3e13542
enskyeing <skyetsatsis@gmail.com>
2024-07-19 21:39:14 -0700

    Remove print statements

--- src/discobase/discobase.py
+++ src/discobase/discobase.py
@@ -12,16 +12,12 @@ class Database:
         @self.bot.event
         async def on_ready() -> None:
             """When bot is online, creates DB server."""
-            print(f'Logged in as {self.bot.user}')
-
             # Create a new server for the DB
             guild = await self.bot.create_guild(name=self.name)
-            print(f'Server {guild.name} created.')
 
             # Get the invite link for the server
             landing_channel = await guild.create_text_channel(name="landing", reason="creating invite")
-            invite = await landing_channel.create_invite(max_age=0, max_uses=0)
-            print(f'Invite link: {invite.url}')
+            invite = await landing_channel.create_invite()
 
         # Initialize the bot with the given token
         self.bot.run(token=bot_token)
