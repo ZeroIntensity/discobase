e951bb449f7fa0e9e3177b0d6cacc32ae8ab4d63
enskyeing <skyetsatsis@gmail.com>
2024-07-19 21:36:40 -0700

    Add type hints

--- src/discobase/discobase.py
+++ src/discobase/discobase.py
@@ -3,15 +3,15 @@ from discord.ext import commands
 
 
 class Database:
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         self.name = name
         intents = discord.Intents.default()
         intents.message_content = True
         self.bot = discord.Client(intents=intents)
 
-    def login(self, bot_token):
+    def login(self, bot_token) -> None:
         @self.bot.event
-        async def on_ready():
+        async def on_ready() -> None:
             """When bot is online, creates DB server."""
             print(f'Logged in as {self.bot.user}')
 
