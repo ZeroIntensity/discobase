821f785f36b8875af32f35c659a83880b2da56f3
enskyeing <skyetsatsis@gmail.com>
2024-07-19 21:51:07 -0700

    Add check for duplicate guild before guild creation

--- src/discobase/discobase.py
+++ src/discobase/discobase.py
@@ -12,12 +12,14 @@ class Database:
         @self.bot.event
         async def on_ready() -> None:
             """When bot is online, creates DB server."""
-            # Create a new server for the DB
-            guild = await self.bot.create_guild(name=self.name)
+            guilds_list = [guild.name for guild in self.bot.guilds]  # List of guild names the bot is in
 
-            # Get the invite link for the server
-            landing_channel = await guild.create_text_channel(name="landing", reason="creating invite")
-            invite = await landing_channel.create_invite()
+            # Create a new server for the DB if not duplicate
+            if self.name not in guilds_list:
+                guild = await self.bot.create_guild(name=self.name)
+                # Get the invite link for the server
+                landing_channel = await guild.create_text_channel(name="landing", reason="creating invite")
+                invite = await landing_channel.create_invite()
 
         # Initialize the bot with the given token
         self.bot.run(token=bot_token)
