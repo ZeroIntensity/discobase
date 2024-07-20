06c4e843786015c95c4756f0dffcad6fe6f60b5e
enskyeing <skyetsatsis@gmail.com>
2024-07-20 00:03:30 -0700

    Fix style conventions

--- src/discobase/discobase.py
+++ src/discobase/discobase.py
@@ -11,17 +11,18 @@ class Database:
         @self.bot.event
         async def on_ready() -> None:
             """When bot is online, creates DB server."""
-            guilds_list = [guild.name for guild in self.bot.guilds]  # List of guild names the bot is in
+            guilds_list = [
+                guild.name for guild in self.bot.guilds
+            ]  # List of guild names the bot is in
 
             # Create a new server for the DB if not duplicate
             if self.name not in guilds_list:
                 guild = await self.bot.create_guild(name=self.name)
                 # Get the invite link for the server
                 landing_channel = await guild.create_text_channel(
-                    name="landing",
-                    reason="creating invite"
+                    name="landing", reason="creating invite"
                 )
-                invite = await landing_channel.create_invite()
+                await landing_channel.create_invite()
 
         # Initialize the bot with the given token
         self.bot.run(token=bot_token)
