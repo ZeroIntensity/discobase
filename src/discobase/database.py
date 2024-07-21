d2dcc0c6f16072203c8b6744cf0561ba84fd6a20
Rubiks14 <3637169+Rubiks14@users.noreply.github.com>
2024-07-21 01:39:17 -0500

    Add create_table to Database class
    I added a property to keep track of the metadata channel and make it
    easier to retrieve data from it or write data to it.
    
    create_table creates a table in the database and all index tables that
    go with it. It then writes the metadata about the table to the metadata
    channel

--- src/discobase/database.py
+++ src/discobase/database.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+import json
 from threading import Thread
+from typing import Dict, Set, Tuple
 
 import discord
 
@@ -18,6 +20,7 @@ class Database:
         intents = discord.Intents.all()
         self.bot = discord.Client(intents=intents)
         self.guild: discord.Guild | None = None
+        self.metadata_channel: discord.TextChannel | None = None
 
         @self.bot.event
         async def on_ready() -> None:
@@ -29,11 +32,63 @@ class Database:
             for guild in self.bot.guilds:
                 if guild.name == self.name:
                     found_guild = guild
+                    break
 
             if not found_guild:
                 self.guild = await self.bot.create_guild(name=self.name)
             else:
                 self.guild = found_guild
+
+            metadata_channel_name = f"{self.name}_metadata"
+            found_channel: discord.TextChannel | None = None
+            for channel in self.guild.text_channels:
+                if channel.name == metadata_channel_name:
+                    found_channel = channel
+                    break
+
+            if not found_channel:
+                self.metadata_channel = await self.guild.create_text_channel(
+                    name=metadata_channel_name
+                )
+            else:
+                self.metadata_channel = found_channel
+
+    async def create_table(
+        self, name: str, fields: Dict[str, Tuple[type, bool]]
+    ) -> None:
+        """
+        Creates a new table and all index tables that go with it. Writes the table metadata.
+
+        name: str - The name of the new table
+        fields: Dict[str, Tuple[type, bool]] - Information about the types of data that will be stored in the database
+            key - Field name
+            value - Tuple containing the type of data to be stored and whether the field is required
+        return None
+        """
+
+        if self.guild is None:
+            raise TypeError(
+                "The bot is not logged in. Please call login before creating a table."
+            )
+
+        primary_table = await self.guild.create_text_channel(name)
+        index_tables: Set[discord.TextChannel] = []
+        for field_name in fields.keys():
+            index_tables.add(
+                await self.guild.create_text_channel(f"{name}-{field_name}")
+            )
+
+        table_metadata = {
+            "name": name,
+            "fields": fields,
+            "table_channel": primary_table.id,
+            "index_channels": set(index_table.id for index_table in index_tables),
+            "current_records": 0,
+            "max_records": 0,
+        }
+
+        message_text = json.dumps(table_metadata)
+        self.metadata_channel.send(message_text)
 
     def login(self, bot_token: str) -> None:
         """
