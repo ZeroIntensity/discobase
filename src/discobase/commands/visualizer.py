c7b1f83f7dc8ee1fe76697c17b62545186e7c75e
Gimpy3887 <gimpy2345@gmail.com>
2024-07-21 14:04:00 -0400

    Add file for visual slash commands

---
+++ src/discobase/commands/visualizer.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class DatabaseVisualizer(commands.Cog):
+    """
+    Slash commands that involve visualizing database information.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command()
+    @app_commands.describe(channel='Choose the database you want to fetch statistics from.')
+    async def status(self,
+                     inter: discord.Interaction,
+                     channel: discord.TextChannel) -> None:
+        """
+        Displays general statistics.
+        """
+        pass
+
+    @app_commands.command()
+    @app_commands.describe(table='Choose the database you want to retrieve the schema from.')
+    async def schema(self,
+                     inter: discord.Interaction,
+                     table: discord.TextChannel) -> None:
+        """
+        Fetches the schema of the channel.
+        """
+        pass
+
+
+async def setup(bot) -> None:
+    await bot.add_cog(DatabaseVisualizer(bot))
