4e1f95690e1891d5abc1466b391601ebcf9c7f02
enskyeing <skyetsatsis@gmail.com>
2024-07-22 09:21:10 -0700

    Add cmds insert and edit

--- src/discobase/cogs/utility.py
+++ src/discobase/cogs/utility.py
@@ -10,9 +10,42 @@ class Utility(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="hello", description="Says hello!")
-    async def hello(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f"Hello {interaction.user}!", ephemeral=True)
+    @app_commands.command(name="insert", description="Insert new data into a table.")
+    @app_commands.describe(
+        table="The table which the data is to be inserted into.",
+        data="The data that is to be inserted."
+    )
+    async def insert(self, interaction: discord.Interaction, table: str, data: str) -> None:
+        """
+        Discord slash command that inserts new data in the specified Table.
+
+        :param interaction: Represents a Discord interaction.
+        :param table: The table which the data is to be inserted into.
+        :param data: The data that is to be inserted.
+        """
+        await interaction.response.send_message(f"I have inserted `{data}` into `{table}` table.")
+
+    @app_commands.command(name="edit", description="Edit a piece of pre-existing data in a table.")
+    @app_commands.describe(
+        table="The table which the data is to be edited.",
+        column="The column in which the data is to be edited.",
+        row="The row in which the data is to be edited.",
+        new_data="The new data that replaces the old data."
+    )
+    async def edit(self, interaction: discord.Interaction, table: str, column: str, row: int,
+                   new_data: str) -> None:
+        """
+        Discord slash command that edits a piece of pre-existing data in a table.
+
+        :param interaction: Represents a Discord interaction.
+        :param table: The table which the data is to be edited.
+        :param column: The column in which the data is to be edited.
+        :param row: The row in which the data is to be edited.
+        :param new_data: The new data that replaces the old data.
+        """
+        await interaction.response.send_message(
+            f"I have edited data in table `{table}` / column `{column}` / row `{row}` to `{new_data}`."
+        )
 
 
 async def setup(bot) -> None:
