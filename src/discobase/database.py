f3082fde830daa3ff8693a0deae1f8450f51769e
enskyeing <skyetsatsis@gmail.com>
2024-07-22 09:08:16 -0700

    Add support for external slash cmds

--- src/discobase/database.py
+++ src/discobase/database.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import asyncio
 from contextlib import asynccontextmanager
 from typing import Type, TypeVar
+from pkgutil import iter_modules
 
 import discord
 import orjson
@@ -54,6 +55,12 @@ class Database:
         Initializes the database server.
         Generally, you don't want to call this manually.
         """
+        # Load external commands
+        for module in iter_modules(path=['cogs'], prefix='cogs.'):
+            await self.bot.load_extension(module.name)
+
+        await self.bot.tree.sync()
+        
         await self.bot.wait_until_ready()
         found_guild: discord.Guild | None = None
         for guild in self.bot.guilds:
