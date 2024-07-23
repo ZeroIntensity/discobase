278a6cd8b9b40369d4d520668bb2bba0c51f5693
Gimpy3887 <gimpy2345@gmail.com>
2024-07-23 10:54:18 -0400

    Revert description to one liner

--- src/discobase/cogs/utility.py
+++ src/discobase/cogs/utility.py
@@ -41,9 +41,7 @@ class Utility(commands.Cog):
             f"I have edited data in table `{table}` / column `{column}` / row `{row}` to `{new_data}`."
         )
 
-    @app_commands.command(
-        description="Modifies a record with a new value."
-    )
+    @app_commands.command(description="Modifies a record with a new value.")
     @app_commands.describe(
         table='Choose the database you want to perform an update on.',
         field='Choose the key you want to update.',
