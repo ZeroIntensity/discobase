0c3f67967c6069bb6d9f7ae48b4ee54a9a511ddf
Gimpy3887 <gimpy2345@gmail.com>
2024-07-23 11:04:17 -0400

    Refactor insert slash command

--- src/discobase/cogs/utility.py
+++ src/discobase/cogs/utility.py
@@ -11,20 +11,7 @@ class Utility(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(
-        description="Insert new data into a table."
-    )
-    async def insert(self, interaction: discord.Interaction, table: str, data: str) -> None:
-        """
-        Discord slash command that inserts new data in the specified Table.
-
-        :param interaction: Represents a Discord interaction.
-        :param table: The table which the data is to be inserted into.
-        :param data: The data that is to be inserted.
-        """
-        await interaction.response.send_message(f"I have inserted `{data}` into `{table}` table.")
-
-    @app_commands.command(name="edit", description="Edit a piece of pre-existing data in a table.")
+    @app_commands.command(description="Insert new data into a table.")
     @app_commands.describe(
         table="The table which the data is to be edited.",
         column="The column in which the data is to be edited.",
