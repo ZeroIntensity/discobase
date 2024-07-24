e1d7eb972fe4630a1635280fd42f1cfcaadef50f
Gimpy3887 <gimpy2345@gmail.com>
2024-07-23 22:32:43 -0400

    Replace old implementation of cog

--- src/discobase/cogs/visualization.py
+++ src/discobase/cogs/visualization.py
@@ -5,51 +5,59 @@ from discord.ext import commands
 
 class Visualization(commands.Cog):
     """
-    Commands to visualize the database.
+    Slash commands to visualize the database's data.
     """
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(
-        name="table", description="Sends a rich embed of the table data."
-    )
-    @app_commands.describe(name="Name of the table")
-    async def table(self, interaction: discord.Interaction, name: str) -> None:
-        """
-        Discord slash command that sends the table data as a rich embed.
-
-        :param interaction: Represents a Discord interaction.
-        :param name: Name of the table.
-
-        :return: None
-        """
+    @app_commands.command(description="View the selected table.")
+    @app_commands.describe(name="The name of the table.")
+    async def table(
+        self,
+        interaction: discord.Interaction,
+        name: discord.TextChannel,
+    ) -> None:
         await interaction.response.send_message(
             f"I am showing you data from the table `{name}`."
         )
 
-    @app_commands.command(
-        name="column", description="Sends a rich embed of the column data."
-    )
+    @app_commands.command(description="View the column data.")
     @app_commands.describe(
-        table="Table the column is in", name="Name of the column"
+        table="The name of the table the column is in.",
+        name="The name of the column.",
     )
     async def column(
-        self, interaction: discord.Interaction, table: str, name: str
+        self,
+        interaction: discord.Interaction,
+        table: discord.TextChannel,
+        name: str,
     ) -> None:
-        """
-        Discord slash command that sends a rich embed of column data.
-
-        :param interaction: Represents a Discord interaction.
-        :param table: Name of the table the column is in.
-        :param name: Name of the column.
-
-        :return: None
-        """
         await interaction.response.send_message(
-            f"I am showing you data from the column `{name}` in table `{table}`."
+            f"I am showing you data from the column `{name}`"
+            f" in table `{table}`."
         )
 
+    @app_commands.command()
+    @app_commands.describe(
+        description="Displays general statistics for the selected table.",
+        channel="Choose the table you want to fetch statistics from.",
+    )
+    async def status(
+        self, interaction: discord.Interaction, channel: discord.TextChannel
+    ) -> None:
+        pass
 
-async def setup(bot) -> None:
+    @app_commands.command()
+    @app_commands.describe(
+        description="Retrieves and displays the schema for the table.",
+        table="Choose the table you want to retrieve the schema from.",
+    )
+    async def schema(
+        self, interaction: discord.Interaction, table: discord.TextChannel
+    ) -> None:
+        pass
+
+
+async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Visualization(bot))
