19e805270f6478290dd3ca532e3580c959c60f05
enskyeing <skyetsatsis@gmail.com>
2024-07-26 00:52:07 -0700

    Update insert cmd draft

--- src/discobase/cogs/utility.py
+++ src/discobase/cogs/utility.py
@@ -14,7 +14,6 @@ class Utility(commands.Cog):
         self.bot = bot
         self.db = self.bot.db
 
-
     @app_commands.command(description="Insert new data into a table.")
     @app_commands.describe(
         table="Choose the table you want to insert the data into.",
@@ -28,7 +27,7 @@ class Utility(commands.Cog):
     ) -> None:
         table_name = table.name.replace("-", " ")
         try:
-            table = [table for table in self.bot.db._tables if table.__disco_name__ == table_name][0]
+            table = [table for table in self.bot.db._tables if table.__disco_name__ == table_name][0]  # Table object
         except IndexError:
             await interaction.response.send_message(f"The table '{table_name} does not exist.")
             return
@@ -38,11 +37,19 @@ class Utility(commands.Cog):
 
         # Check if column name exists
         # If it does then insert the data into it
-        for k, v in data_dict.keys():
-            table.k = v
+        # for k, v in data_dict.items():
+        #     if table.hasattr(k):
+        #         table.k = v
+        try:
+            new_entry = table(**data_dict)
+        except TypeError:
+            await interaction.response.send_message(
+                f"The keys in your data did not match the column names for {table_name}"
+            )
+            return
 
         # save db
-        table.save()
+        new_entry.save()
 
         await interaction.response.send_message(
             f"I have inserted `{data}` into `{table}` table."
